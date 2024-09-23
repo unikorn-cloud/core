c9afe3ac1142caa655ea89d3b2e69bcb6b558b83
Simon Murray <spjmurray@yahoo.co.uk>
2024-09-23 11:06:57 +0100

    Make UUID Generator Library (#80)
    THings other than APIs need to be able to generate Kubernetes name
    compatible UUIDs, mainly for initial system configuration.

---
+++ pkg/util/resource.go
@@ -0,0 +1,35 @@
+/*
+Copyright 2024 the Unikorn Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package util
+
+import (
+	"unicode"
+
+	"k8s.io/apimachinery/pkg/util/uuid"
+)
+
+// GenerateResourceID creates a valid Kubernetes name from a UUID.
+func GenerateResourceID() string {
+	for {
+		// NOTE: Kubernetes UUIDs are based on version 4, aka random,
+		// so the first character will be a letter eventually, like
+		// a 6/16 chance: tl;dr infinite loops are... improbable.
+		if id := uuid.NewUUID(); unicode.IsLetter(rune(id[0])) {
+			return string(id)
+		}
+	}
+}
